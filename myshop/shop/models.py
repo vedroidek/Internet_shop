92f740ce1a40f9400130ac8e06d181cd267a73a5
MaximShaybakov <momon_boton@mail.ru>
2023-06-28 02:47:16 +0500

    Added model 'Product'

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -17,3 +17,24 @@ class Category(models.Model):
         return self.name
     
     
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=256)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        oredering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+        ]
+        
+    def __str__(self) -> str:
+        return self.name
