458a16e1bf1f17694436fdcab031c73af8dc6b4d
MaximShaybakov <momon_boton@mail.ru>
2023-06-29 13:44:50 +0500

    Added shop.view products_detail
    The Product instance is retrieved by id and slug. slug to specify to generate a friendly search optimization URL.

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -12,3 +12,8 @@ def product_list(request, category_slug=None):
     return render(request, 'shop/product/list.html', {'category': category,
                                                       'categories': categories,
                                                       'products': products})
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
+    return render(request, 'shop/product/detail.html', {'product': product})
