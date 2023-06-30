fbb527402dd0ff35a6ad16150ebc0c9167bcdf12
MaximShaybakov <momon_boton@mail.ru>
2023-07-01 04:13:17 +0500

    Save cart state
    Adding or saving items to the cart in the current session.

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -12,3 +12,19 @@ class Cart:
             # save empty cart to session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+
+    def add(self, product: str, quantity: int = 1, override_quantity: bool = False) -> None:
+        """ Add a product to the cart or update its quantity. """
+        product_id = f'{product.id}'
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0, 'price': f'{product.price}'}
+
+        if override_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def save(self):
+        #  mark the session as "modified" to ensure it persists
+        self.session.modified = True
