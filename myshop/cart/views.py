0f95d7bcdb9551bd831842595aa2077b79209e5d
MaximShaybakov <momon_boton@mail.ru>
2023-07-02 00:38:59 +0500

    Fix views.py
    The cart_add, cart_remove, cart_detail functions have been moved to the views of their application.

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_POST
+from Internet_shop.myshop.cart.cart import Cart
+from Internet_shop.myshop.cart.forms import CartAddProductForm
+from Internet_shop.myshop.shop.models import Product
 
-# Create your views here.
+
+@require_POST
+def cart_add(request, product_id):
+    """ Adding items to the cart or updating their quantity. """
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
+    return redirect('cart:cart_detail')
+
+
+@require_POST
+def cart_remove(request, product_id):
+    """ Remove product from cart by id. """
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    cart.remove(product)
+    return redirect('cart:cart_detail')
+
+
+def cart_detail(request):
+    """ Template link function for cart_add and cart_remove. """
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})
--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,9 +1,5 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.views.decorators.http import require_POST
-
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
-from ..cart.cart import Cart
-from ..cart.forms import CartAddProductForm
 
 
 def product_list(request, category_slug=None):
@@ -24,29 +20,3 @@ def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     return render(request, 'shop/product/detail.html', {'product': product})
 
-
-@require_POST
-def cart_add(request, product_id):
-    """ Adding items to the cart or updating their quantity. """
-    cart = Cart(request)
-    product = get_object_or_404(Product, id=product_id)
-    form = CartAddProductForm(request.POST)
-    if form.is_valid():
-        cd = form.cleaned_data
-        cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
-    return redirect('cart:cart_detail')
-
-
-@require_POST
-def cart_remove(request, product_id):
-    """ Remove product from cart by id. """
-    cart = Cart(request)
-    product = get_object_or_404(Product, id=product_id)
-    cart.remove(product)
-    return redirect('cart:cart_detail')
-
-
-def cart_detail(request):
-    """ Template link function for cart_add and cart_remove. """
-    cart = Cart(request)
-    return render(request, 'cart/detail.html', {'cart': cart})
