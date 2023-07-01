416ac7288386e79b105ca0cd2229d66a3e492e19
MaximShaybakov <momon_boton@mail.ru>
2023-07-01 23:35:39 +0500

    Create func cart_add in views.py
    Adding items to the cart. If these products are already in the basket, then if necessary, change their quantity.

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.decorators.http import require_POST
+
 from .models import Category, Product
+from ..cart.cart import Cart
+from ..cart.forms import CartAddProductForm
 
 
 def product_list(request, category_slug=None):
@@ -17,3 +21,15 @@ def product_list(request, category_slug=None):
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)
     return render(request, 'shop/product/detail.html', {'product': product})
+
+
+@require_POST
+def cart_add(request, product_id):
+    """ Adding items to the cart or updating their quantity. """
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=product_id)
+    form = CartAddProductForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
+    return redirect('cart:cart_detail')
