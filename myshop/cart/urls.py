27f7fa3664ea6bfc161e1a9cd3530f682690de15
MaximShaybakov <momon_boton@mail.ru>
2023-07-02 03:13:40 +0500

    Added base template

--- myshop/cart/urls.py
+++ myshop/cart/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 app_name = 'cart'
+
 urlpatterns = [
     path('', views.cart_detail, name='cart_detail'),
     path('add/<int:product_id>/', views.cart_add, name='cart_add'),
