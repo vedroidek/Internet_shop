acb2ca17963faa6eb3d55ff54cd6585bd9c79aee
MaximShaybakov <momon_boton@mail.ru>
2023-07-02 02:21:47 +0500

    Fix field ChoiceField to TypedChoiceFild
    Coercion of the string value of the value to an integer.

--- myshop/cart/forms.py
+++ myshop/cart/forms.py
@@ -6,5 +6,5 @@ PRODUCT_QUANTITY_CHOICES = [(i, f'{i}') for i in range(1, 21)]
 
 class CartAddProductForm(forms.Form):
     """ Form for added products in cart. """
-    quantity = forms.ChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
+    quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES, coerce=int)
     override = forms.BooleanField(required=False, initial=False, widget=forms.HiddenInput)
