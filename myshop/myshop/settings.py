47fb436066043904b398e64db0d0176c213b5972
MaximShaybakov <momon_boton@mail.ru>
2023-07-05 00:35:56 +0500

    Bugfix. Added 'cart' app to INSTALLED_APPS

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +60,7 @@ ROOT_URLCONF = 'myshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
