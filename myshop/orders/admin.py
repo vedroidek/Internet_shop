22dee92e7f2207371e64bd77001ac3c194e90861
MaximShaybakov <momon_boton@mail.ru>
2023-07-05 04:29:33 +0500

    Added display of information about orders on the admin page

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    """ The model is included inline when displaying
+    order information on the admin page. """
+    model = OrderItem
+    raw_id_fields = ['product']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """ Display order information on the admin page. """
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address',
+                    'postal_code', 'city', 'paid', 'created', 'updated']
+    list_filter = ['paid', 'created', 'updated']
+    inlines = [OrderItemInline]
