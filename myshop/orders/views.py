c6fb2c257d2ff0c6f636964466b19e65eaf3ff42
MaximShaybakov <momon_boton@mail.ru>
2023-07-06 02:17:31 +0500

    Syntax error fix

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -18,6 +18,6 @@ def order_create(request):
 
                 cart.clear()
                 return render(request, 'orders/order/created.html', {'order': order})
-        else:
-            form = OrderCreateForm()
-        return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
