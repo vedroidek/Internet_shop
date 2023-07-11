1af1188f1e9cfeca5896e60d6c771534207dc400
MaximShaybakov <momon_boton@mail.ru>
2023-07-11 18:06:20 +0500

    Added a link to the order in the admin panel
    Defined the function of creating a link to the order displayed in the administrative panel.

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import Order, OrderItem
 
 
@@ -9,10 +11,23 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
+def order_stripe_payment(obj):
+    url = obj.get_stripe_url()
+    if obj.stripe_id:
+        html = f'<a href="{url}" target="_blank">{obj.stripe_id}</a>'
+        return mark_safe(html)
+    return ''
+
+
+order_stripe_payment.short_description = 'Stripe payment'
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     """ Display order information on the admin page. """
     list_display = ['id', 'first_name', 'last_name', 'email', 'address',
-                    'postal_code', 'city', 'paid', 'created', 'updated', 'stripe_id']
+                    'postal_code', 'city', 'paid', order_stripe_payment,
+                    'created', 'updated', 'stripe_id']
+
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from shop.models import Product
 
 
