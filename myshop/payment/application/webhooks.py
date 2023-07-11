c787ca010b12e4ec00f47aafba3d2661b443dddc
MaximShaybakov <momon_boton@mail.ru>
2023-07-11 11:32:35 +0500

    Added payment verification functionality
    It checks that the event is "successfully completed". If successful, it checks if the session mode is payment mode "mode=payment" . The id attribute of the client session is retrieved. If this id exists, then we mark the order as "paid".

--- myshop/payment/application/webhooks.py
+++ myshop/payment/application/webhooks.py
@@ -21,4 +21,15 @@ def stripe_webhook(request):
         #  Invalid signature
         return HttpResponse(status=400)
 
+    if event.type == 'checkout.session.completed':
+        session = event.data.object
+        if session.mode == 'payment' and session.payment_status == 'paid':
+            try:
+                order = Order.objects.get(id=session.client_reference_id)
+            except Order.DoesNotExist:
+                return HttpResponse(status=400)
+            #  marking the order as "paid"
+            order.paid = True
+            order.save()
+
     return HttpResponse(status=200)
