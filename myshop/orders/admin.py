7e906da23ebf96fcf334dc8214e85f492b2b0fcd
MaximShaybakov <momon_boton@mail.ru>
2023-07-11 19:29:55 +0500

    Added functionality for the administration panel
    Added the function to export the list of orders to a CSV file.

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -1,6 +1,9 @@
-from django.contrib import admin
-from django.utils.safestring import mark_safe
+import csv
+import datetime
 
+from django.contrib import admin
+from django.http import HttpResponse
+from django.utils.safestring import mark_safe
 from .models import Order, OrderItem
 
 
@@ -22,6 +25,30 @@ def order_stripe_payment(obj):
 order_stripe_payment.short_description = 'Stripe payment'
 
 
+def export_to_csv(modeladmin, request, queryset):
+    opts = modeladmin.model._meta
+    content_disposition = f'attachment; filename={opts.verbose_name}.csv'
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = content_disposition
+    writer = csv.writer(response)
+    fields = [field for field in opts.get_fields() if not field.many_to_many and not field.one_to_many]
+    #  write header to csv file
+    writer.writerow([field.verbose_name for field in fields])
+    #  write data string
+    for obj in queryset:
+        data_row = []
+        for field in fields:
+            value = getattr(obj, field.name)
+            if isinstance(value, datetime.datetime):
+                value = value.strftime('%d/%m/%Y')
+            data_row.append(value)
+        writer.writerow(data_row)
+    return response
+
+
+export_to_csv.short_description = 'Export to CSV'
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     """ Display order information on the admin page. """
@@ -31,3 +58,4 @@ class OrderAdmin(admin.ModelAdmin):
 
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
