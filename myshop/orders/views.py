a850928221096fb11ac0d242bd19823213012490
MaximShaybakov <momon_boton@mail.ru>
2023-07-18 00:56:18 +0500

    Added PDF generation view
    To admin only. The view generates a PDF invoice at the request of the administrator.

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,5 +1,9 @@
+import weasyprint
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.shortcuts import reverse
+from django.template.loader import render_to_string
+from django.conf import settings
 from .tasks import order_created
 from cart.cart import Cart
 from orders.forms import OrderCreateForm
@@ -35,3 +39,16 @@ def admin_order_detail(request, order_id):
      users with administrator status. """
     order = get_object_or_404(Order, id=order_id)
     return render(request, 'admin/orders/order/detail.html', {'order': order})
+
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    """ The view generates a PDF invoice for the order.
+     To admin only."""
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html',
+                            {'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(response, stylesheets=[weasyprint.CSS(settings.STATIC_ROOT / 'css/pdf.css')])
+    return response
