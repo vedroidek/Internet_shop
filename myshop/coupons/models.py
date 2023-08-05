71ca278f6c1c874ef641ba46490eb0a788d30780
MaximShaybakov <momon_boton@mail.ru>
2023-08-06 01:11:12 +0500

    Added coupons model
    Contains coupon information.

--- myshop/coupons/models.py
+++ myshop/coupons/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Coupon(models.Model):
+    """ Coupon model.
+     Contains data:
+     a unique coupon code,
+     the beginning and end of the coupon,
+     percentage discount,
+     whether the coupon is valid.
+     """
+    code = models.CharField(max_length=64, unique=True)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    discount = models.IntegerField(validators=[MinValueValidator(0),
+                                               MaxValueValidator(100)],
+                                   help_text='Percentage value (0 to 100)')
+    active = models.BooleanField()
+
+    def __str__(self):
+        return self.code
