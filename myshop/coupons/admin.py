185c03ba6afc9bae878a1161d5072435511de75a
MaximShaybakov <momon_boton@mail.ru>
2023-08-22 01:20:45 +0500

    Added documentations for coupons model from admin panel

--- myshop/coupons/admin.py
+++ myshop/coupons/admin.py
@@ -5,6 +5,8 @@ from .models import Coupon
 
 @admin.register(Coupon)
 class CouponAdmin(admin.ModelAdmin):
+    """ Displaying the coupon model in the admin panel.
+     Ability to manually manage coupons. """
     list_display = ['code', 'valid_from', 'valid_to', 'discount', 'active']
     list_filter = ['active', 'valid_from', 'valid_to']
     search_fields = ['code']
