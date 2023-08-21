6e2aad824ab26e78aca113e98e4e2083f5bdd7d2
MaximShaybakov <momon_boton@mail.ru>
2023-08-22 03:05:10 +0500

    Added view to apply coupon
    Search for a coupon, check its validity, save in session and apply the discount to the order.

--- myshop/coupons/views.py
+++ myshop/coupons/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils import timezone
+from django.views.decorators.http import require_POST
+from .models import Coupon
+from .forms import CouponApplyForm
 
-# Create your views here.
+
+@require_POST
+def coupon_apply(request):
+    """ Find an existing coupon that is currently valid.
+    Code compliance check. When a coupon with information is found
+    discount information will be stored in the user session.
+    When you save an order, a coupon is attached to that order. """
+    now = timezone.now()
+    form = CouponApplyForm(request.POST)
+    if form.is_valid():
+        code = form.cleaned_data['code']
+        try:
+            coupon = Coupon.objects.get(code__iexact=code, valid_from__lte=now, valid_to___gte=now, active=True)
+            request.session['coupon_id'] = coupon.id
+        except Coupon.DoesNotExist:
+            request.session['coupon_id'] = None
+    return redirect('cart:cart_detail')
